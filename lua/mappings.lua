812e8377fd8724f27abf9062943606a2700a8a09
Damars <damarsepti627@gmail.com>
2024-04-16 11:24:57 +0700

    tab behaviour

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -5,6 +5,20 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
+map("i", "jj", "<ESC>")
 map("i", "jk", "<ESC>")
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+-- Copilot
+map('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
+  expr = true,
+  replace_keycodes = false
+})
+vim.g.copilot_no_tab_map = true
+
+-- Telescope
+-- map
+
+-- change copilot keymap to ALT+L
+
